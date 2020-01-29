0ab360ce7a7137d5c17eff0da546af9204b71723
peter <peter.mckee@gmail.com>
2020-01-29 07:02:41 -0600

    save

--- src/playground/index.js
+++ src/playground/index.js
@@ -1,70 +1,39 @@
-//
-//  RAW DOM METHODS
-//
+import strata from '../strata'
 
-function element( tag ) {
-  return document.createElement( tag )
-}
+const { el, tn, div, form, label, textbox, button } = strata
 
-function text( txt ) {
-  return document.createTextNode( txt )
-}
-//
-// *********************************
-//
-function div() {}
-function label( text ) {}
+// Full Name
+const lblFullName = label( 'Full Name' )
+const txtFullName = textbox()
 
-const spn = label( 'Full Name' )
+const divFullName = div()
+divFullName.append( lblFullName, txtFullName )
+
+// Email
+const lblEmail = label( 'Email' )
+const txtEmail = textbox()
+
+const divEmail = div()
+divEmail.append( lblEmail, txtEmail )
+
+// Password
+const lblPassword = label( 'Password' )
+const txtPassword = textbox()
+
+const divPassword = div()
+divPassword.append( lblPassword, txtPassword )
+
+// Done Button
+const btnDone = button( 'Done' )
+const divDoneButton = div()
+divDoneButton.append( btnDone )
+
+// Form
+const userForm = form()
+userForm.method = "POST"
+userForm.action = "new_user.html"
+
+userForm.append( divFullName, divEmail, divPassword, divDoneButton )
 
 const root = document.getElementById( 'root' )
-root.appendChild( div )
-
-// <div>
-//  <span>Full Name</span>
-// </div>
-
-// const form = element( 'form' )
-// form.method = "POST"
-// form.action = "new_user.html"
-
-// const spnFullName = element( 'span' )
-// spnFullName.appendChild( text( 'Full Name' ) )
-
-
-
-// const spnEmail = element( 'span' )
-// spnEmail.appendChild( text( 'Email' ) )
-
-// const spnPassword = element( 'span' )
-// spnPassword.appendChild( text( 'Password' ) )
-
-// const inptFullName = element( 'input' )
-
-
-// const inptEmail = element( 'input' )
-// const inptPassword = element( 'input' )
-// const btnDone = element( 'button' )
-// btnDone.appendChild( text( 'Done' ) )
-
-// const divFullName = element( 'div' )
-// const divEmail = element( 'div' )
-// const divPassword = element( 'div' )
-// const divDoneButton = element( 'div' )
-
-// divFullName.appendChild( spnFullName )
-// divFullName.appendChild( inptFullName )
-// form.appendChild( divFullName )
-
-// divEmail.appendChild( spnEmail )
-// divEmail.appendChild( inptEmail )
-// form.appendChild( divEmail )
-
-// divPassword.appendChild( spnPassword )
-// divPassword.appendChild( inptPassword )
-// form.appendChild( divPassword )
-
-// divDoneButton.appendChild( btnDone )
-// form.appendChild( divDoneButton )
-
-
+root.appendChild( userForm )
--- src/strata/index.js
+++ src/strata/index.js
@@ -1,7 +1,58 @@
 import logger from './logger'
 
 const strata = {
-  logger
+  logger,
+
+  el,
+  tn,
+
+  div,
+  form, 
+  input,
+
+  label,
+  textbox, 
+  button
 }
+
+function el( tag ) {
+  return document.createElement( tag )
+}
+
+function tn( txt ) {
+  return document.createTextNode( txt )
+}
+
+
+
+function div() {
+  return el( 'div' )
+}
+
+function form() {
+  return el( 'form' )
+}
+
+function input( type ) {
+  const input = el( 'input' )
+  input.type = type
+  return input
+}
+
+
+function label( txt ) {
+  return el( 'span' ).appendChild( tn( txt ) )
+}
+
+function textbox() {
+  return input( 'textbox' )
+}
+
+function button( label ) {
+  const btn = el( 'button' )
+  btn.appendChild( tn( label ) )
+  return btn
+}
+
 
 export default strata
