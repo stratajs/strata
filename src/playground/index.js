a763fdfc29069a4feb04891bd9203de7c4a4da67
peter <peter.mckee@gmail.com>
2020-01-30 16:58:31 -0600

    save

--- src/playground/index.js
+++ src/playground/index.js
@@ -2,11 +2,11 @@ import strata from '../strata'
 
 const { el, tn, div, form, span, label, textbox, button } = strata
 
-const e = el( 'div', { className: "foo" }, [
-  el( 'span', {}, [
-    tn( 'Hello, World!' )
+const d = div( { className: "foo" }, [
+  span( {}, [
+    tn( 'This is our DIV' )
   ])
 ])
 
 const root = document.getElementById( 'root' )
-root.appendChild( e )
+root.appendChild( d )
